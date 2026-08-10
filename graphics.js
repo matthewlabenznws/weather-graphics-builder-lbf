a31f6b8ce69559cbfe8c4a1260a1c48ce24c66a6
matthewlabenznws <matthew.labenz@noaa.gov>
2026-08-10 12:16:25 -0500

    Update graphics.js

--- graphics.js
+++ graphics.js
@@ -16,3 +16,69 @@ map.addControl(
     new mapboxgl.NavigationControl(),
     "top-right"
 );
+
+map.on("load", () => {
+
+    // ============================================================
+    // COUNTY BOUNDARIES
+    // ============================================================
+
+    map.addSource("counties", {
+        type: "geojson",
+        data: "https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json"
+    });
+
+    map.addLayer({
+        id: "county-boundaries",
+        type: "line",
+        source: "counties",
+
+        paint: {
+            "line-color": "#000000",
+            "line-width": [
+                "interpolate",
+                ["linear"],
+                ["zoom"],
+                4, 0.4,
+                6, 0.8,
+                8, 1.2,
+                10, 1.5
+            ],
+
+            "line-opacity": 0.9
+        }
+    });
+
+
+    // ============================================================
+    // STATE BOUNDARIES
+    // ============================================================
+
+    map.addSource("states", {
+        type: "geojson",
+        data: "https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json"
+    });
+
+    map.addLayer({
+        id: "state-boundaries",
+        type: "line",
+        source: "states",
+
+        paint: {
+            "line-color": "#000000",
+
+            "line-width": [
+                "interpolate",
+                ["linear"],
+                ["zoom"],
+                4, 1.2,
+                6, 1.8,
+                8, 2.2,
+                10, 2.5
+            ],
+
+            "line-opacity": 1.0
+        }
+    });
+
+});
