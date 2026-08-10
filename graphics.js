1c6defcfc0369e2e6cd1aec46bb8e1491daa5de1
matthewlabenznws <matthew.labenz@noaa.gov>
2026-08-10 11:56:03 -0500

    Create graphics.js

---
+++ graphics.js
@@ -0,0 +1,18 @@
+mapboxgl.accessToken = "pk.eyJ1IjoibWF0dGhld2xhYmVuejciLCJhIjoiY21zbjhxZ3ZkMXBoNDJ3cHl5eG5uNzlpZCJ9.UBy4k84SejJzzu2VF0TtcA";
+
+const map = new mapboxgl.Map({
+    container: "map",
+
+    style: "mapbox://styles/mapbox/light-v11",
+
+    center: [-100.75, 41.1],
+
+    zoom: 6,
+
+    preserveDrawingBuffer: true
+});
+
+map.addControl(
+    new mapboxgl.NavigationControl(),
+    "top-right"
+);
